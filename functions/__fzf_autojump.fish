bfba958b85368856819bc5d1b11acf642fe0c26a
Roman Inflianskas <infroma@gmail.com>
2018-03-14 10:38:36 +0300

    Fix plugin

--- functions/__fzf_autojump.fish
+++ functions/__fzf_autojump.fish
@@ -3,9 +3,8 @@ function __fzf_autojump
   or set -l FZF_AUTOJUMP_COMMAND "autojump-list-dirs"
   set -q FZF_TMUX_HEIGHT; or set FZF_TMUX_HEIGHT 40%
   # Fish hangs if the command before pipe redirects (2> /dev/null)
-  fish -c "$FZF_AUTOJUMP_COMMAND" | __fzfcmd -m $FZF_AUTOJUMP_OPTS | read -l select
-  if not test -z "$select"
-    cd "$select"
-  end
+  fish -c "$FZF_AUTOJUMP_COMMAND" | eval (__fzfcmd) +m $FZF_AUTOJUMP_OPTS > $TMPDIR/fzf.result
+  [ (cat $TMPDIR/fzf.result | wc -l) -gt 0 ]
+  and cd (cat $TMPDIR/fzf.result)
   commandline -f repaint
 end
