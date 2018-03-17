572bf126c100f7a10f74d1468b93bc4d535f9555
Roman Inflianskas <infroma@gmail.com>
2018-03-17 09:38:11 +0300

    Don't show cluttered directories

--- functions/__fzf_autojump.fish
+++ functions/__fzf_autojump.fish
@@ -3,7 +3,7 @@ function __fzf_autojump
   or set -l FZF_AUTOJUMP_COMMAND "autojump-list-dirs"
   set -q FZF_TMUX_HEIGHT; or set FZF_TMUX_HEIGHT 40%
   # Fish hangs if the command before pipe redirects (2> /dev/null)
-  fish -c "$FZF_AUTOJUMP_COMMAND" | eval (__fzfcmd) +m $FZF_AUTOJUMP_OPTS > $TMPDIR/fzf.result
+  eval $FZF_AUTOJUMP_COMMAND | eval (__fzfcmd) +m $FZF_AUTOJUMP_OPTS > $TMPDIR/fzf.result
   [ (cat $TMPDIR/fzf.result | wc -l) -gt 0 ]
   and cd (cat $TMPDIR/fzf.result)
   commandline -f repaint
